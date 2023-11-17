c486e684f2a50e8ede65432af9befa06444df9e6
Kuizuo <hi@kuizuo.cn>
2023-11-18 00:19:57 +0800

    feat(api): repl mode

---
+++ apps/api/src/repl.ts
@@ -0,0 +1,13 @@
+import { repl } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const replServer = await repl(AppModule)
+  replServer.setupHistory('.nestjs_repl_history', (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+}
+bootstrap()
