96b41abb35ea508f0885f5a2c0916d69b5e9981d
Kuizuo <hi@kuizuo.cn>
2023-12-01 20:07:12 +0800

    refactor: decorators

--- apps/api/src/shared/helper/helper.module.ts
+++ apps/api/src/shared/helper/helper.module.ts
@@ -2,11 +2,9 @@ import { Global, Module, type Provider } from '@nestjs/common'
 
 import { CronService } from './cron.service'
 import { QQService } from './qq.service'
-import { SmsService } from './sms.service'
 
 const providers: Provider[] = [
   CronService,
-  SmsService,
   QQService,
 ]
 
